d09e5849c79c69cafc7e33e5a17ec2a4ac1629d3
Ceasar Cyrillus <te15ceacyr@falufri.se>
2017-04-07 19:53:25 +0200

    Added upload script and form

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from werkzeug import secure_filename
 from format_phonegap import format_data
 from bs4 import BeautifulSoup
 import xml.etree.cElementTree as ET
@@ -9,9 +10,19 @@ import requests
 
 app = Flask("app")
 
+	
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      f = request.files['file']
+      f.save(secure_filename(f.filename))
+      return 'file uploaded successfully'
+
 @app.route("/")
 def home():
-    return "Hello World!"
+	with open("index.html", "r") as f:
+		page_content = f.read()
+	return page_content
 
 #if __name__ == "__main__":
-app.run("0.0.0.0")
+app.run(debug=True)
