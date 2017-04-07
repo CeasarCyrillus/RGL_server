7914bbe7b24e4acc17c6e111e49364355adef48b
Ceasar Cyrillus <te15ceacyr@falufri.se>
2017-04-07 19:43:14 +0200

    Added main formating script to app.py

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask
+from format_phonegap import format_data
+from bs4 import BeautifulSoup
+import xml.etree.cElementTree as ET
+from bs4 import BeautifulSoup
+import codecs
+import os
+import requests
 
-app = Flask("webapp")
+app = Flask("app")
 
 @app.route("/")
 def home():
     return "Hello World!"
 
-if __name__ == "__main__":
-    app.run("0.0.0.0")
+#if __name__ == "__main__":
+app.run("0.0.0.0")
